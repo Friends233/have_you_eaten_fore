1cfc01b85128e77c6b6018659a41c50785ba22f8
Friends233 <friends233@foxmail.com>
2021-02-19 11:12:34 +0800

    增加全局样式

--- src/components/EatenHeader.vue
+++ src/components/EatenHeader.vue
@@ -15,38 +15,45 @@ export default class EatenHeader extends Vue {
 
   protected render() {
     return (
-      <el-menu default-active="activeIndex" class="el-menu-demo" mode="horizontal" onSelect={this.handleSelect}>
-        <el-submenu index="1">
-          <template slot="title">我的工作台</template>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
-          <el-menu-item index="1-3">选项3</el-menu-item>
-        </el-submenu>
-        <el-submenu index="2">
-          <template slot="title">我的工作台</template>
-          <el-menu-item index="2-1">选项1</el-menu-item>
-          <el-menu-item index="2-2">选项2</el-menu-item>
-          <el-menu-item index="2-3">选项3</el-menu-item>
-          <el-submenu index="2-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="2-4-1">选项1</el-menu-item>
-            <el-menu-item index="2-4-2">选项2</el-menu-item>
-            <el-menu-item index="2-4-3">选项3</el-menu-item>
+      <div class="eaten-header">
+        <div class="eaten-header-top">1</div>
+        <div class="eaten-header-mid">
+          <div class="eaten-header-mid-logo">logo</div>
+          <div class="eaten-header-mid-search">search</div>
+        </div>
+        <el-menu default-active="activeIndex" class="eaten-header-nav" mode="horizontal" onSelect={this.handleSelect}>
+          <el-submenu index="1">
+            <template slot="title">我的工作台1</template>
+            <el-menu-item index="1-1">选项1</el-menu-item>
+            <el-menu-item index="1-2">选项2</el-menu-item>
+            <el-menu-item index="1-3">选项3</el-menu-item>
           </el-submenu>
-        </el-submenu>
-        <el-submenu index="1">
-          <template slot="title">我的工作台</template>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
-          <el-menu-item index="1-3">选项3</el-menu-item>
-        </el-submenu>
-        <el-submenu index="1">
-          <template slot="title">我的工作台</template>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
-          <el-menu-item index="1-3">选项3</el-menu-item>
-        </el-submenu>
-      </el-menu>
+          <el-submenu index="2">
+            <template slot="title">我的工作台2</template>
+            <el-menu-item index="2-1">选项1</el-menu-item>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+            <el-menu-item index="2-3">选项3</el-menu-item>
+            <el-submenu index="2-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="2-4-1">选项1</el-menu-item>
+              <el-menu-item index="2-4-2">选项2</el-menu-item>
+              <el-menu-item index="2-4-3">选项3</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title">我的工作台3</template>
+            <el-menu-item index="3-1">选项1</el-menu-item>
+            <el-menu-item index="3-2">选项2</el-menu-item>
+            <el-menu-item index="3-3">选项3</el-menu-item>
+          </el-submenu>
+          <el-submenu index="4">
+            <template slot="title">我的工作台4</template>
+            <el-menu-item index="4-1">选项1</el-menu-item>
+            <el-menu-item index="4-2">选项2</el-menu-item>
+            <el-menu-item index="4-3">选项3</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </div>
     )
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+import '@/styles/index.scss'
+
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
