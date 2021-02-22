8dad9e109074b2be3086cab3e4503c02a2f5e6d6
Friends233 <friends233@foxmail.com>
2021-02-22 15:45:18 +0800

    增加config文件，设置打包路径和配置跨域

---
+++ vue.config.js
@@ -0,0 +1,24 @@
+/*
+ * @Description: 
+ * @Author: Friends233
+ */
+module.exports = {
+  publicPath:'./', // 设置静态打包路径
+  // devServer 选项单独配置
+  devServer: {
+    https: false,
+    open: true,
+    host: 'localhost',
+    port: 8080,
+    proxy: {
+      '/api': {
+        target: "http://192.168.0.21:8080",
+        changeOrigin: true,
+        ws: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
+}
