87a39c7a2551070b4508cca37a2030f351080c60
Friends233 <friends233@foxmail.com>
2021-03-02 16:20:07 +0800

    添加评论页面数据结构

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -16,6 +16,16 @@ interface Goods {
   price?: number;
 }
 
+interface UserApp {
+  id?: string;
+  name?: string;
+  avatar?: string;
+  rating?: number;
+  content?: string;
+  time?: string;
+  imgUrl?: Array<string>;
+}
+
 @Component({
   components: { EatenHeader, EatenFooter, Star }
 })
@@ -51,13 +61,50 @@ export default class Shop extends Vue {
     '朋友聚餐(20)',
     '价格实惠(7)'
   ]
-  // 评价的图片列表
-  appraisalImg: Array<string> = [
-    'https://p0.meituan.net/shaitu/c1c9fcf5b6105e68aa3eab70ac27ef03884501.jpg',
-    'https://p0.meituan.net/shaitu/b47d954c79fb2ac797bb71226170d0995057150.jpg',
-    'https://p0.meituan.net/shaitu/bb45ac8e7fbf4016028030f42acb6c1e799653.jpg'
+  appraisals: Array<UserApp> = [
+    {
+      id: '133',
+      name: 'haole',
+      rating: 4.8,
+      content:
+        '吃的很饱，寿司很好吃，海带丝也很好吃， 凑字数 孟子曰：天将降大任于斯人也，必先苦其心志，劳其筋骨，饿其体肤，空乏其身，行拂乱其所为，所以动心忍性，增益其所不能。太史公曰：古者富贵而名摩灭，不可胜记，唯倜傥非常之人称焉。盖文王拘而演《周易》；仲尼厄而作《春秋》；屈原放逐，乃赋《离骚》；左丘失明，厥有《国语》；孙子膑脚，《兵法》修列；不韦迁蜀，世传《吕览》；韩非囚秦，《说难》《孤愤》；《诗》三百篇，大底圣贤发愤之所为作也。',
+      time: '2021年2月21日',
+      avatar: 'https://www.dpfile.com/ugc/user/anonymous.png',
+      imgUrl: [
+        'https://p0.meituan.net/shaitu/c1c9fcf5b6105e68aa3eab70ac27ef03884501.jpg',
+        'https://p0.meituan.net/shaitu/b47d954c79fb2ac797bb71226170d0995057150.jpg',
+        'https://p0.meituan.net/shaitu/bb45ac8e7fbf4016028030f42acb6c1e799653.jpg'
+      ]
+    },
+    {
+      id: '2333',
+      name: 'haole',
+      rating: 4.8,
+      content:
+        '吃的很饱，寿司很好吃，海带丝也很好吃， 凑字数 孟子曰：天将降大任于斯人也，必先苦其心志，劳其筋骨，饿其体肤，空乏其身，行拂乱其所为，所以动心忍性，增益其所不能。太史公曰：古者富贵而名摩灭，不可胜记，唯倜傥非常之人称焉。盖文王拘而演《周易》；仲尼厄而作《春秋》；屈原放逐，乃赋《离骚》；左丘失明，厥有《国语》；孙子膑脚，《兵法》修列；不韦迁蜀，世传《吕览》；韩非囚秦，《说难》《孤愤》；《诗》三百篇，大底圣贤发愤之所为作也。',
+      time: '2021年2月21日',
+      avatar: 'https://www.dpfile.com/ugc/user/anonymous.png',
+      imgUrl: [
+        'https://p0.meituan.net/shaitu/c1c9fcf5b6105e68aa3eab70ac27ef03884501.jpg',
+        'https://p0.meituan.net/shaitu/b47d954c79fb2ac797bb71226170d0995057150.jpg',
+        'https://p0.meituan.net/shaitu/bb45ac8e7fbf4016028030f42acb6c1e799653.jpg'
+      ]
+    },
+    {
+      id: '3333',
+      name: 'haole',
+      rating: 4.8,
+      content:
+        '吃的很饱，寿司很好吃，海带丝也很好吃， 凑字数 孟子曰：天将降大任于斯人也，必先苦其心志，劳其筋骨，饿其体肤，空乏其身，行拂乱其所为，所以动心忍性，增益其所不能。太史公曰：古者富贵而名摩灭，不可胜记，唯倜傥非常之人称焉。盖文王拘而演《周易》；仲尼厄而作《春秋》；屈原放逐，乃赋《离骚》；左丘失明，厥有《国语》；孙子膑脚，《兵法》修列；不韦迁蜀，世传《吕览》；韩非囚秦，《说难》《孤愤》；《诗》三百篇，大底圣贤发愤之所为作也。',
+      time: '2021年2月21日',
+      avatar: 'https://www.dpfile.com/ugc/user/anonymous.png',
+      imgUrl: [
+        'https://p0.meituan.net/shaitu/c1c9fcf5b6105e68aa3eab70ac27ef03884501.jpg',
+        'https://p0.meituan.net/shaitu/b47d954c79fb2ac797bb71226170d0995057150.jpg',
+        'https://p0.meituan.net/shaitu/bb45ac8e7fbf4016028030f42acb6c1e799653.jpg'
+      ]
+    }
   ]
-  checkboxGroup1 = []
 
   protected render() {
     return (
@@ -160,39 +207,41 @@ export default class Shop extends Vue {
                 })}
               </ul>
               <ul class="shop-card-appraisal-content">
-                <li>
-                  <div>
-                    <img width="60" src="https://www.dpfile.com/ugc/user/anonymous.png" />
-                  </div>
-                  <div>
-                    <p class="user-name">a***1</p>
-                    <div class="mid-line">
-                      <star num={4.8} size={'13'}></star>
-                      <span class="time">2021年2月21日</span>
-                    </div>
-                    <span class="appraisal-content">
-                      吃的很饱，寿司很好吃，海带丝也很好吃， 凑字数
-                      孟子曰：天将降大任于斯人也，必先苦其心志，劳其筋骨，饿其体肤，空乏其身，行拂乱其所为，所以动心忍性，增益其所不能。太史公曰：古者富贵而名摩灭，不可胜记，唯倜傥非常之人称焉。盖文王拘而演《周易》；仲尼厄而作《春秋》；屈原放逐，乃赋《离骚》；左丘失明，厥有《国语》；孙子膑脚，《兵法》修列；不韦迁蜀，世传《吕览》；韩非囚秦，《说难》《孤愤》；《诗》三百篇，大底圣贤发愤之所为作也。
-                    </span>
-                    <div class="img-list">
-                      {this.appraisalImg.map((i) => {
-                        return (
-                          <el-image
-                            style="width: 140px; height: 140px"
-                            src={i}
-                            preview-src-list={this.appraisalImg}></el-image>
-                        )
-                      })}
-                    </div>
-                    <div class="like">
-                      <el-checkbox text-color="">
-                        <span>
-                          <i class="el-icon-thumb"></i>点赞
-                        </span>
-                      </el-checkbox>
-                    </div>
-                  </div>
-                </li>
+                {this.appraisals.map((item: UserApp) => {
+                  return (
+                    <li key={item.id}>
+                      <div>
+                        <img width="60" src={item.avatar} />
+                      </div>
+                      <div>
+                        <p class="user-name">{item.name}</p>
+                        <div class="mid-line">
+                          <star num={item.rating} size={'13'}></star>
+                          <span class="time">{item.time}</span>
+                        </div>
+                        <span class="appraisal-content">{item.content}</span>
+                        <div class="img-list">
+                          {item.imgUrl &&
+                            item.imgUrl.map((i: string) => {
+                              return (
+                                <el-image
+                                  style="width: 140px; height: 140px"
+                                  src={i}
+                                  preview-src-list={item.imgUrl}></el-image>
+                              )
+                            })}
+                        </div>
+                        <div class="like">
+                          <el-checkbox text-color="">
+                            <span>
+                              <i class="el-icon-thumb"></i>点赞
+                            </span>
+                          </el-checkbox>
+                        </div>
+                      </div>
+                    </li>
+                  )
+                })}
               </ul>
             </el-tab-pane>
             <el-tab-pane label="商家信息">商家信息</el-tab-pane>
