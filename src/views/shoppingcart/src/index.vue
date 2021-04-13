63c3321b0bc2e32c92ce5a1cc7e7162a6d96930d
Friends233 <friends233@foxmail.com>
2021-04-13 17:09:34 +0800

    feat:购物车

--- src/views/shoppingcart/src/index.vue
+++ src/views/shoppingcart/src/index.vue
@@ -5,20 +5,32 @@
 <script lang="tsx">
 import { Component, Vue } from 'vue-property-decorator'
 
-
 @Component({
-  components: {  }
+  components: {}
 })
 export default class ShoppingCart extends Vue {
- 
+  private visible = false
+
+  handleClose(){
+    console.log("close")
+  }
+
   protected render() {
     return (
-      <div>购物车</div>
+      <div>
+      <button onClick={()=>this.visible = true}>打开</button>
+        <el-drawer
+          title="购物车"
+          visible={this.visible}
+          {...{ on: { 'update:visible': (e: any) => (this.visible = e) } }}
+          onBeforeClose={this.handleClose}>
+          <span>购物车</span>
+        </el-drawer>
+      </div>
     )
   }
 }
 </script>
 <style scoped lang="scss">
 @import '@/styles/constant.scss';
-
 </style>
