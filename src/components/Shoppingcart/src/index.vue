eb3ad15a782f73f8b606f68dbf5c5c65df8c6689
Friends233 <Friends233@foxmail.com>
2021-04-27 15:40:50 +0800

    feat:购物车响应式

--- src/components/Shoppingcart/src/index.vue
+++ src/components/Shoppingcart/src/index.vue
@@ -3,7 +3,7 @@
  * @Author: Friends233
 -->
 <script lang="tsx">
-import { Component, Vue,Prop } from 'vue-property-decorator'
+import { Component, Vue, Prop } from 'vue-property-decorator'
 import { ShoppingCartConifg } from './index'
 
 @Component({
@@ -109,7 +109,7 @@ export default class ShoppingCart extends Vue {
     console.log('close')
   }
 
-  showDialog(){
+  showDialog() {
     this.visible = true
   }
 
@@ -126,6 +126,22 @@ export default class ShoppingCart extends Vue {
     e.stopPropagation()
   }
 
+  checkAll() {
+    if (this.foodCheckbox.includes(false)) {
+      this.foodCheckbox = this.foodCheckbox.map(() => {
+        return true
+      })
+    } else {
+      this.foodCheckbox = this.foodCheckbox.map(() => {
+        return false
+      })
+    }
+  }
+
+  clearAll() {
+    this.data = []
+  }
+
   get isDisabled() {
     return !this.foodCheckbox.includes(true)
   }
@@ -138,6 +154,13 @@ export default class ShoppingCart extends Vue {
           visible={this.visible}
           {...{ on: { 'update:visible': (e: any) => (this.visible = e) } }}
           onBeforeClose={this.handleClose}>
+          <div class="shoppingcart-btn">
+            <a onClick={this.checkAll}>全选</a>
+            <a onClick={this.clearAll}>清空</a>
+          </div>
+          <p v-show={this.data && this.data.length === 0} class="desc">
+            无商品
+          </p>
           <ul class="food">
             {this.data.map((item, index) => {
               return (
@@ -171,8 +194,25 @@ export default class ShoppingCart extends Vue {
 .shoppingcart {
   ::v-deep .el-drawer {
     width: 450px !important;
+    .el-drawer__header {
+      margin-bottom: 15px;
+      border-bottom: 1px double #c0c4cc;
+      padding: 10px 20px;
+    }
     .el-drawer__body {
       position: relative;
+    }
+  }
+  &-btn {
+    display: flex;
+    justify-content: space-between;
+    padding: 5px 20px;
+    margin-bottom: 15px;
+    a {
+      cursor: pointer;
+      &:last-child {
+        color: red;
+      }
     }
   }
   .el-button {
@@ -213,7 +253,7 @@ export default class ShoppingCart extends Vue {
         position: relative;
         .label {
           font-weight: 700;
-          font-size: 18px;
+          font-size: 1.13rem;
           span {
             &:last-child {
               margin-left: 15px;
@@ -224,17 +264,27 @@ export default class ShoppingCart extends Vue {
           position: absolute;
           right: 20px;
           top: 4px;
-          font-size: 16px;
+          font-size: 1rem;
           color: #666666;
           cursor: pointer;
         }
         p {
-          font-size: 14px;
+          font-size: 0.88rem;
           color: #666666;
           margin: 8px 0;
         }
       }
     }
+  }
+}
+@media screen and (max-width: 600px) {
+  .shoppingcart {
+    ::v-deep .el-drawer {
+      width: 100% !important;
+    }
+  }
+  .food {
+    max-height: 450px !important;
   }
 }
 </style>
